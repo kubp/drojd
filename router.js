f8459b91470c1239de2a4d2dc49405a8bed63489
kubp <dolezal000@gmail.com>
2016-01-23 14:29:54 +0100

    Load default route path from config

--- router.js
+++ router.js
@@ -2,11 +2,11 @@ module.exports = function(app) {
 
 
   Auth = require("./src/Auth");
-  
   Section = require("./src/Section");
-Page = require("./src/Page");
-Blog = require("./src/Blog");
-BlogSection = require("./src/BlogSection");
+  Page = require("./src/Page");
+  Blog = require("./src/Blog");
+  BlogSection = require("./src/BlogSection");
+  Main = require("./src/Main");
 
   var handlers = {
     page: new Page(),
@@ -14,63 +14,65 @@ BlogSection = require("./src/BlogSection");
     //user: new User(),
     section: new Section(),
     blog: new Blog(),
-    blog_section: new BlogSection()
+    blog_section: new BlogSection(),
+    main: new Main()
   };
 
+   app.get(config.api_url+'/', handlers.main.get);
 
 /* Section */
 
-  app.get('/api/section/:id', handlers.section.get);
+  app.get(config.api_url+'/section/:id', handlers.section.get);
 
-  app.get('/api/section/', handlers.section.getAll);
+  app.get(config.api_url+'/section/', handlers.section.getAll);
 
-  app.delete('/api/section/:id', handlers.auth.auth,handlers.section.remove);
+  app.delete(config.api_url+'/section/:id', handlers.auth.auth,handlers.section.remove);
 
-  app.put('/api/section/:id', handlers.auth.auth,handlers.section.update);
+  app.put(config.api_url+'/section/:id', handlers.auth.auth,handlers.section.update);
 
 /* Page */
 
-  app.get('/api/page/', handlers.page.getAll);
+  app.get(config.api_url+'/page/', handlers.page.getAll);
 
-  app.get('/api/search/page/', handlers.page.search);
+  app.get(config.api_url+'/search/page/', handlers.page.search);
 
-  app.get('/api/page/:id', handlers.page.get);
+  app.get(config.api_url+'/page/:id', handlers.page.get);
 
-  app.post('/api/page/', handlers.auth.auth, handlers.page.set);
+  app.post(config.api_url+'/page/', handlers.auth.auth, handlers.page.set);
 
-  app.delete('/api/page/:id', handlers.auth.auth, handlers.page.remove);
+  app.delete(config.api_url+'/page/:id', handlers.auth.auth, handlers.page.remove);
 
-  app.put('/api/page/:id', handlers.auth.auth, handlers.page.update);
+  app.put(config.api_url+'/page/:id', handlers.auth.auth, handlers.page.update);
 
 /* Blog */
 
-  app.get('/api/blog/', handlers.blog.getAll);
+  app.get(config.api_url+'/blog/', handlers.blog.getAll);
 
-  app.get('/api/blog/:id', handlers.blog.get);
+  app.get(config.api_url+'/blog/:id', handlers.blog.get);
 
-  app.post('/api/blog/', handlers.auth.auth, handlers.blog.set);
+  app.post(config.api_url+'/blog/', handlers.auth.auth, handlers.blog.set);
 
-  app.delete('/api/blog/:id', handlers.auth.auth, handlers.blog.remove);
+  app.delete(config.api_url+'/blog/:id', handlers.auth.auth, handlers.blog.remove);
 
-  app.put('/api/blog/:id', handlers.auth.auth, handlers.blog.update);
+  app.put(config.api_url+'/blog/:id', handlers.auth.auth, handlers.blog.update);
 
 
 /* Blog Section */
 
-  app.get('/api/blog_section/:id', handlers.blog_section.get);
+  app.get(config.api_url+'/blog_section/:id', handlers.blog_section.get);
 
-  app.post('/api/blog_section/', handlers.auth.auth, handlers.blog_section.set);
+  app.post(config.api_url+'/blog_section/', handlers.auth.auth, handlers.blog_section.set);
 
-  app.delete('/api/blog_section/:id', handlers.auth.auth, handlers.blog_section.remove);
+  app.delete(config.api_url+'/blog_section/:id', handlers.auth.auth, handlers.blog_section.remove);
 
-  app.put('/api/blog_section/:id', handlers.auth.auth, handlers.blog_section.update);
+  app.put(config.api_url+'/blog_section/:id', handlers.auth.auth, handlers.blog_section.update);
 
 /* Auth */
 
-  app.post('/api/login/', handlers.auth.login);
+  app.post(config.api_url+'/login/', handlers.auth.login);
 
-  app.get('/api/login/add', handlers.auth.add);
+  app.get(config.api_url+'/login/add', handlers.auth.add);
 
-  app.get('/api/verify/:apikey', handlers.auth.auth, handlers.auth.verifyUser);
+  app.get(config.api_url+'/verify/:apikey', handlers.auth.auth, handlers.auth.verifyUser);
 
 }
