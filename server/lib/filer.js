08749a98a3d02487de76ecb0a4ed5da6909ae0c5
kubp <dolezal000@gmail.com>
2016-02-10 20:24:53 +0100

    Add auth to upload route

--- server/lib/filer.js
+++ server/lib/filer.js
@@ -1,3 +1,6 @@
+var Auth = require("./../src/Auth");
+var auth = new Auth();
+
 module.exports = function(app) {
 
   var multer  = require('multer')
@@ -7,7 +10,7 @@ module.exports = function(app) {
   var fs = require('fs');
   var path = require('path');
 
-  app.post('/api/image/', upload.single('file'), function (req, res, next) {
+  app.post('/api/image/', auth.auth, upload.single('file'), function (req, res, next) {
     try{
     fs.rename(req.file.path, __dirname+'/../../uploads/images/'+req.file.originalname, function(err) {
         if ( err ) return res.status(403).json({status:"error"})
@@ -19,7 +22,7 @@ module.exports = function(app) {
 
   })
 
-  app.get('/api/image', function(req, res) {
+  app.get('/api/image', auth.auth, function(req, res) {
     fs.readdir(__dirname + '/../../uploads/images', function(err, items) {
     res.json(items)
     });
